8d544b735c2a98dfa7419b726bc8e16c97cf0779
laura1513 <laurajorques2003@gmail.com>
2023-11-22 20:25:10 +0100

    Avanzo

--- app/src/main/java/com/example/mvvmcombatepokemon/Pokemon2Fragment.java
+++ app/src/main/java/com/example/mvvmcombatepokemon/Pokemon2Fragment.java
@@ -8,7 +8,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,86 +15,84 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Pokemon1Fragment extends Fragment {
+public class Pokemon2Fragment extends Fragment {
     Button botonValidar;
     Button guardar;
-    EditText hp1;
-    EditText nombre1;
-    EditText ataque1;
-    EditText defensa1;
-    EditText atEsp1;
-    EditText defEsp1;
+    EditText hp2;
+    EditText nombre2;
+    EditText ataque2;
+    EditText defensa2;
+    EditText atEsp2;
+    EditText defEsp2;
     Pokemon pokemon;
     NavController navController;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pokemon1, container, false);
+        return inflater.inflate(R.layout.fragment_pokemon2, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
         super.onViewCreated(view, savedInstanceState);
-        navController = Navigation.findNavController(view);
-        PokemonViewModel pokemonViewModel = new ViewModelProvider(this).get(PokemonViewModel.class);
-        botonValidar = view.findViewById(R.id.botonValidar1);
-        guardar = view.findViewById(R.id.botonGuardarPok1);
 
+        PokemonViewModel pokemonViewModel = new ViewModelProvider(this).get(PokemonViewModel.class);
+        botonValidar = view.findViewById(R.id.botonValidar2);
+        guardar = view.findViewById(R.id.botonGuardarPok1);
         botonValidar.setOnClickListener(v -> {
             boolean err = false;
 
             String nombre;
-            nombre1 = view.findViewById(R.id.etnombre1);
+            nombre2 = view.findViewById(R.id.etnombre2);
             int hp = 0;
-            hp1 = view.findViewById(R.id.ethp2);
+            hp2 = view.findViewById(R.id.ethp2);
             int ataque = 0;
-            ataque1 = view.findViewById(R.id.etataque1);
+            ataque2 = view.findViewById(R.id.etataque2);
             int defensa = 0;
-            defensa1 = view.findViewById(R.id.etdefensa1);
+            defensa2 = view.findViewById(R.id.etdefensa2);
             int ataqueEsp = 0;
-            atEsp1 = view.findViewById(R.id.etatEsp1);
+            atEsp2 = view.findViewById(R.id.etatEsp2);
             int defensaEsp = 0;
-            defEsp1 = view.findViewById(R.id.etdefEsp1);
+            defEsp2 = view.findViewById(R.id.etdefEsp2);
 
             //Nombre del pokemon
-            nombre = nombre1.toString();
+            nombre = nombre2.toString();
 
             //HP del pokemon, salta la excepción en caso de no ser un numero
             try {
-                hp = Integer.parseInt(hp1.toString());
+                hp = Integer.parseInt(hp2.toString());
             } catch (NumberFormatException ex) {
-                hp1.setError("Hay que introducir un número");
+                hp2.setError("Hay que introducir un número");
                 err = true;
             }
 
             //Ataque del pokemon, salta la excepción en caso de no ser un numero
             try {
-                ataque = Integer.parseInt(ataque1.toString());
+                ataque = Integer.parseInt(ataque2.toString());
             } catch (NumberFormatException ex) {
-                ataque1.setError("Hay que introducir un número");
+                ataque2.setError("Hay que introducir un número");
                 err = true;
             }
             //Defensa del pokemon, salta la excepción en caso de no ser un numero
             try {
-                defensa = Integer.parseInt(defensa1.toString());
+                defensa = Integer.parseInt(defensa2.toString());
             } catch (NumberFormatException ex) {
-                defensa1.setError("Hay que introducir un número");
+                defensa2.setError("Hay que introducir un número");
                 err = true;
             }
             //Ataque especial del pokemon, salta la excepción en caso de no ser un numero
             try {
-                ataqueEsp = Integer.parseInt(atEsp1.toString());
+                ataqueEsp = Integer.parseInt(atEsp2.toString());
             } catch (NumberFormatException ex) {
-                atEsp1.setError("Hay que introducir un número");
+                atEsp2.setError("Hay que introducir un número");
                 err = true;
             }
 
             //Defensa especial del pokemon, salta la excepción en caso de no ser un numero
             try {
-                defensaEsp = Integer.parseInt(defEsp1.toString());
+                defensaEsp = Integer.parseInt(defEsp2.toString());
             } catch (NumberFormatException ex) {
-                defEsp1.setError("Hay que introducir un número");
+                defEsp2.setError("Hay que introducir un número");
                 err = true;
             }
 
@@ -113,7 +110,7 @@ public class Pokemon1Fragment extends Fragment {
         guardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navController.navigate(R.id.action_pokemon1Fragment_to_pokemon2Fragment);
+                //navController.navigate(R.id.action_inicioFragment_to_pokemon1Fragment);
             }
         });
     }
